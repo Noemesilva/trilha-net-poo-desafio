acfa014a097c499d3e17693755c73e4d62be1f26
Noeme Silva <noeme16@gmail.com>
2024-05-21 21:33:06 -0300

    Implementação das classes Nokia e iPhone no projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"12546", modelo:"78568", imei:"1111111", memoria: 56);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone(numero:"568956", modelo:"363636", imei: "58585858", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
